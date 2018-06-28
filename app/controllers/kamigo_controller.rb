96af84f8dc1d2fface481997326148a807e5feb0
Laibit <ghostwolf90@gmail.com>
2018-06-28 23:39:54 +0800

    add response

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -3,32 +3,7 @@ class KamigoController < ApplicationController
     protect_from_forgery with: :null_session
 
     def webhook
-        # Line Bot API初始化物件
-        client = Line::Bot::Client.new { |config|
-            config.channel_secret = '3b4829231d6b2f234316683d1dca79fc'
-            config.channel_token = 'Mjuod9qwt2oSVms1uuFrk0Y4RW4B3sqRge+AnCKzyHAjjfGEUBxbBWO2Rc9xPYkmrhVjHlekKL+dmZZi1rYmasAevDtG9JtNmzbbPYZCzB56TjAzpimlLFIziVXoSSDT4udc0XvTJur5F+1+MN7cqgdB04t89/1O/w1cDnyilFU='
-        }
-  
-        # 取得 reply token
-        reply_token = params['events'][0]['replyToken']
-        p "==這裡是replay_token==="
-        p reply_token
-        p "======================"
-
-        # 設定回覆訊息
-        message = {
-            type: 'text',
-            text: '好哦～好哦～'
-        }
-
-        # 傳送訊息
-        response = client.reply_message(reply_token, message)
-        p '========='
-        p response
-        p response.body
-        p '========='
-    
-        # 回應 200
-        head :ok
+        render plain: params
+   
     end
 end
