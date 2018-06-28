1cb0dccc27c8354fa692e7846ec7657b123c96e9
Laibit <ghostwolf90@gmail.com>
2018-06-28 23:45:33 +0800

    add rrrd

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/kamigo/webhook', to: 'kamigo#webhook'
-  get '/kamigo/sent_request',to: 'kamigo#sent_request'
+  post '/kamigo/webhook', to: 'kamigo#webhook'  
 end
