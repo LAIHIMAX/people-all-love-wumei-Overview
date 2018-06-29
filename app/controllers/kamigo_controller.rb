2b5050f12172f1890ec23bc9a625badf68df08b0
Laibit <ghostwolf90@gmail.com>
2018-06-30 00:09:53 +0800

    推齊1

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -10,11 +10,47 @@ class KamigoController < ApplicationController
         # 關鍵字回覆
         reply_text = keyword_reply(received_text) if reply_text.nil?
 
+        # 推齊
+        reoly_text = echo2(channel_id, received_text) if reply_text.nil?
+        save_to_received(channel_id, received_text)
+        save_to_reply(channel_id, reply_text)
+
         # 傳送訊息到 line
         response = reply_to_line(reply_text)
      
          # 回應 200
          head :ok
+    end
+
+    # 頻道ID
+    def channel_id
+        source = params['events'][0]['source']
+        return source['groupId'] unless source['groupId'].nil?
+        return source['rommId'] unless source['roomId'].nil?
+        source['userId']
+    end
+
+    # 儲存對話
+    def save_to_received(channel_id, received_text)
+        return if received_text.nil?
+        Received.create(channel_id: channel_id, text: received_text)    
+    end
+
+    # 儲存回應
+    def save_to_reply(channel_id, reply_text)
+        return if reply_text.nil?
+        Reply.create(channel_id: channel_id, text: reply_text)
+    end
+
+    def echo2(channel_id, received_text)
+        # 如果在channel_id最近沒有人講過received_text 烏梅就不回應
+        recent_received_texts = Received.where(channel_id: channel_id).last(5)&.pluck(:text)
+        return nil unless received_text.in? recent_received_texts
+
+        # 如果在channel_id 烏梅上一句回應是received_text 烏梅就不回應
+        last_reply_text = Reply.where(channel_id: channel_id).last&.text
+        return nil if last_reply_text == received_text
+        received_text
     end
 
     # 學說話
