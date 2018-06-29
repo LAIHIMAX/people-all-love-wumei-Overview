19e2a5f0c589596932d7009900505a3a921bcaa0
Laibit <ghostwolf90@gmail.com>
2018-06-29 09:30:37 +0800

    精簡程式碼

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -4,32 +4,47 @@ class KamigoController < ApplicationController
     protect_from_forgery with: :null_session
 
     def webhook
-        # Line Bot API初始化物件
-        client = Line::Bot::Client.new { |config|
-             config.channel_secret = '3b4829231d6b2f234316683d1dca79fc'
-             config.channel_token = 'Mjuod9qwt2oSVms1uuFrk0Y4RW4B3sqRge+AnCKzyHAjjfGEUBxbBWO2Rc9xPYkmrhVjHlekKL+dmZZi1rYmasAevDtG9JtNmzbbPYZCzB56TjAzpimlLFIziVXoSSDT4udc0XvTJur5F+1+MN7cqgdB04t89/1O/w1cDnyilFU='
-         }
-   
-         # 取得 reply token
-         reply_token = params['events'][0]['replyToken']
-         p "==這裡是replay_token==="
-         p reply_token
-         p "======================"
- 
-         # 設定回覆訊息
-         message = {
-             type: 'text',
-             text: '好哦～好哦～'
-         }
- 
-         # 傳送訊息
-         response = client.reply_message(reply_token, message)
-         p '========='
-         p response
-         p response.body
-         p '========='
+        # 設定回覆文字
+        reply_text = keyword_reply(received_text)
+
+         # 傳送訊息到 line
+         response = reply_to_line(reply_text)
      
          # 回應 200
          head :ok
+    end
+
+    # Line Bot API初始化物件
+    def line        
+        return @line unless @line.nil?
+        @line = Line::Bot::Client.new { |config|
+           config.channel_secret = '3b4829231d6b2f234316683d1dca79fc'
+           config.channel_token = 'Mjuod9qwt2oSVms1uuFrk0Y4RW4B3sqRge+AnCKzyHAjjfGEUBxbBWO2Rc9xPYkmrhVjHlekKL+dmZZi1rYmasAevDtG9JtNmzbbPYZCzB56TjAzpimlLFIziVXoSSDT4udc0XvTJur5F+1+MN7cqgdB04t89/1O/w1cDnyilFU='
+       }
+    end
+
+    #傳送訊息到line
+    def reply_to_line(reply_text)
+        # 取得 reply token
+        reply_token = params['events'][0]['replyToken']
+
+        # 設定回覆訊息
+        message = {
+            type: 'text',
+            text: reply_text
+        }
+        
+        # 傳送訊息
+        line.reply_message(reply_token, message)        
+    end
+
+    #取得對方說的話
+    def received_text
+        params['events'][0]['message']['text']
+    end
+
+    #關鍵字回覆
+    def keyword_reply(received_text)
+        received_text
     end
 end
