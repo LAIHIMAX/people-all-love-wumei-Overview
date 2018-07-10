381cbd07f70f1c468f70935145ac4e672ba8f001
Laibit <ghostwolf90@gmail.com>
2018-07-10 09:17:48 +0800

    天氣2

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -39,7 +39,7 @@ class KamigoController < ApplicationController
          head :ok
     end
 
-    def get_weather
+    def get_weather(received_text)
         return nil unless received_text.include? '天氣'
         upload_to_imgur(get_weather_from_cwb)
     end
